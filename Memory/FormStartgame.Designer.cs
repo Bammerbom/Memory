3ca623a6449c0c220e095289523b67dad4954f43
snellejelle99 <snellejelle99jb@gmail.com>
2017-10-13 14:18:32 +0200

    achtergrond kleur wijziging

--- Memory/FormStartgame.Designer.cs
+++ Memory/FormStartgame.Designer.cs
@@ -57,6 +57,7 @@
             // titel
             // 
             this.titel.AutoSize = true;
+            this.titel.BackColor = System.Drawing.Color.Transparent;
             this.titel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titel.Location = new System.Drawing.Point(248, 10);
             this.titel.Name = "titel";
