fda7641ee744a8a571cf7ff4cf08af45c17813f5
JorunnO <oost1704@student.nhl.nl>
2017-10-13 14:09:05 +0200

    Cleanup Startscherm

--- Memory/FormStartgame.cs
+++ Memory/FormStartgame.cs
@@ -16,12 +16,49 @@ namespace Memory
             InitializeComponent();
         }
 
-        private void FormStartgame_Load(object sender, EventArgs e) {
-            
+        private void FormStartgame_Load(object sender, EventArgs e)
+        {
+            comboGametype.Text = "Kies spelmodus";
+            comboSpelgrootte.Visible = false;
+            labelSpelgrootte.Visible = false;
+            textboxNaam1.Visible = false;
+            textboxNaam2.Visible = false;
+            labelNaam1.Visible = false;
+            labelNaam2.Visible = false;
         }
 
         private void gametype_SelectedIndexChanged(object sender, EventArgs e) {
             Console.WriteLine(comboGametype.Text);
+            if(comboGametype.Text == "Kies spelmodus")
+            {
+                comboSpelgrootte.Visible = false;
+                labelSpelgrootte.Visible = false;
+                textboxNaam1.Visible = false;
+                textboxNaam2.Visible = false;
+                labelNaam1.Visible = false;
+                labelNaam2.Visible = false;
+            }
+            if(comboGametype.Text == "Singleplayer")
+            {
+                comboSpelgrootte.Visible = true;
+                labelSpelgrootte.Visible = true;
+                textboxNaam1.Visible = true;
+                labelNaam1.Text = "Naam invullen";
+                labelNaam1.Visible = true;
+                textboxNaam2.Visible = false;
+                labelNaam2.Visible = false;
+            }
+            if (comboGametype.Text == "Multiplayer Local" || comboGametype.Text == "Multiplayer Online")
+            {
+                comboSpelgrootte.Visible = true;
+                labelSpelgrootte.Visible = true;
+                textboxNaam1.Visible = true;
+                textboxNaam2.Visible = true;
+                labelNaam1.Text = "Naam 1 invullen";
+                labelNaam1.Visible = true;
+                labelNaam2.Visible = true;
+            }
+
         }
 
         private void buttonTerug_Click(object sender, EventArgs e) {
@@ -30,12 +67,12 @@ namespace Memory
 
         private void buttonStart_Click(object sender, EventArgs e) {
             switch (comboGametype.Text) {
-                case "Singleplayer":
-                    string naam = inputANaam.Text;
+                default:
+                    string naam = textboxNaam1.Text;
                     if(naam == null || naam.Length == 0) {
                         naam = "Speler 1";
                     }
-                    switch (comboASpelgrootte.Text) {
+                    switch (comboSpelgrootte.Text) {
                         //2x2 2x3 2x4 3x4 4x4
                         case "2x2":
                             GameSingleplayer.Start(2, 2, naam);
@@ -53,13 +90,13 @@ namespace Memory
                             GameSingleplayer.Start(4, 4, naam);
                             break;
                         default:
-                            MessageBox.Show("Voer een spelgrootte in", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            MessageBox.Show("Voer de grootte van het speelveld in", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                             return;
                     }
                     this.Close();
                     break;
-                default:
-                    MessageBox.Show("Voer een gamemode in", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                case "Kies spelmodus":
+                    MessageBox.Show("Voer een spelmodus in", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
             }
         }
