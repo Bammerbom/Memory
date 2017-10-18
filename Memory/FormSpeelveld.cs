14d9d905eda0d3d6d81a22eb69db94c2f7ec954a
JorunnO <oost1704@student.nhl.nl>
2017-10-18 10:38:21 +0200

    Dynamisch speelveld finished

--- Memory/FormSpeelveld.cs
+++ Memory/FormSpeelveld.cs
@@ -20,33 +20,50 @@ namespace Memory
             double ySizeTemp = 263;
             InitializeComponent();
             PictureBox[,] Kaart = new PictureBox[BaseGame.Width, BaseGame.Height];
-            if (BaseGame.Width == 2 && BaseGame.Height == 2) //speelveld 2x2
+            if (BaseGame.Width == 2 && BaseGame.Height == 2) //speelveld 2x2 vergroot de kaarten met factor 2
             {
                 xSizeTemp *= 2;
                 ySizeTemp *= 2;
             }
-            else if (BaseGame.Width == 3 && BaseGame.Height == 2) //speelveld 2x3
+            else if (BaseGame.Width == 3 && BaseGame.Height == 2) //speelveld 2x3 vergroot de kaarten met 1.5
             {
-                xSizeTemp *= 1.25;
-                ySizeTemp *= 1.25;
+                xSizeTemp *= 1.5;
+                ySizeTemp *= 1.5;
             }
-
-            int yLocation = 5;
-            for (int y = 0; y < BaseGame.Height; y++)
+            else if (BaseGame.Width == 4 && BaseGame.Height == 2) //speelveld 2x4 vergroot de kaarten met 1.15
             {
-                int xLocation = 366;
+                yTussenruimte = -18;
+                xSizeTemp *= 1.15;
+                ySizeTemp *= 1.15;
+            }
+            else if (BaseGame.Width == 4 && BaseGame.Height == 3) //speelveld 3x4 vergroot de kaarten met 1.15
+            {
+                yTussenruimte = -18;
+                xSizeTemp *= 1.15;
+                ySizeTemp *= 1.15;
+            }
+            else if (BaseGame.Width == 4 && BaseGame.Height == 4) //speelveld 4x4 geen vergroting
+            {
+                yTussenruimte = -18;
+                xSizeTemp *= 1;
+                ySizeTemp *= 1;
+            }
+            int yLocation = 5;
+            for (int y = 0; y < BaseGame.Height; y++) //voert alle kaarten in met goede tussenruimtes en juiste locatie
+            {
+                int xLocation = 366; //begin locatie
                 for (int x = 0; x < BaseGame.Width; x++)
                 {
-                    string Kaartnaam = "Kaart" + x + "" + y;
-                    Kaart[x, y] = new PictureBox();
+                    string Kaartnaam = "Kaart" + x + "" + y; //juiste naam geven
+                    Kaart[x, y] = new PictureBox(); //maakt nieuwe picturebox
                     Kaart[x, y].Name = Kaartnaam;
-                    Kaart[x, y].Size = new Size(Convert.ToInt32(xSizeTemp), Convert.ToInt32(ySizeTemp));
-                    Kaart[x, y].Image = Properties.Resources.KaartVoorkant;
+                    Kaart[x, y].Size = new Size(Convert.ToInt32(xSizeTemp), Convert.ToInt32(ySizeTemp)); //juiste size per speelveld 
+                    Kaart[x, y].Image = Properties.Resources.KaartVoorkant; 
                     Kaart[x, y].BackColor = Color.Transparent;
                     Kaart[x, y].Visible = true;
                     Kaart[x, y].Anchor = AnchorStyles.Left;
                     Kaart[x, y].SizeMode = PictureBoxSizeMode.Zoom;
-                    Kaart[x, y].Location = new Point(xLocation, yLocation);
+                    Kaart[x, y].Location = new Point(xLocation, yLocation); //nieuwe locatie
                     Kaart[x, y].BringToFront();
                     string eventnaam = Kaartnaam + "_Click";
                     int x2 = x;
@@ -55,15 +72,10 @@ namespace Memory
                         BaseGame.KaartKlik(x2, y2);
                     });
                     this.Controls.Add(Kaart[x, y]);
-                    xLocation += Convert.ToInt32(xSizeTemp) + Convert.ToInt32(xTussenruimte);
+                    xLocation += Convert.ToInt32(xSizeTemp) + Convert.ToInt32(xTussenruimte); //nieuw x coördinaat
                 }
-                yLocation += Convert.ToInt32(ySizeTemp) + Convert.ToInt32(yTussenruimte);
+                yLocation += Convert.ToInt32(ySizeTemp) + Convert.ToInt32(yTussenruimte); //nieuw y coördinaat
             }
-
-
-
-
-
         }
 
         private void Button_Exit_Click(object sender, EventArgs e) {
@@ -72,7 +84,6 @@ namespace Memory
 
         private void Button_Opslaan_Click(object sender, EventArgs e) {
             SaveGameManager.SaveEnEnd();
-            
         }
     }
 }
