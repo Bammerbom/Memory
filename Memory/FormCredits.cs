4330864e0698e2b5f67471f4dd216d5a89d292a7
snellejelle99 <snellejelle99jb@gmail.com>
2017-11-01 15:45:51 +0100

    summary credits af

--- Memory/FormCredits.cs
+++ Memory/FormCredits.cs
@@ -18,15 +18,25 @@ namespace Memory
         }
 
       
-
+        /// <summary>
+        /// zet de achtergrond van de form opde juiste foto voor het juiste thema.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FormCredits_Load(object sender, EventArgs e)
         {
             this.BackgroundImage = (Bitmap)Properties.Resources.ResourceManager.GetObject(ManagerThema.Themaprefix + "CreditsAchtergrond");
         }
-
+        /// <summary>
+        /// closed en disposed de form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Picturebox1Credits_Click(object sender, EventArgs e)
         {
             this.Close();
+            this.Dispose();
+            GC.Collect();
         }
     }
 }
